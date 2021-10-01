476ee9011a8b3e05ff2a41be1e50306f19b8199e
Jaclyn Paul <jaclyn@jaclynpaul.com>
2021-10-01 13:04:53 -0400

    fetch rss feed contents and generate html to list five latest posts

--- rss-aggregator.js
+++ rss-aggregator.js
@@ -1,52 +1,37 @@
-$ = document.getElementById.bind(document);
+const corsAnywhereAppend = 'https://cors-anywhere.herokuapp.com/'
+const jpcomRssUrl = corsAnywhereAppend + 'https://www.jaclynpaul.com/feed/'
+const adhdhsRssUrl = 'https://adhdhomestead.net/feed/'
 
-function showAdvanced() {
-   document.body.classList.toggle("show-advanced");
-   $("showopt").value = document.body.classList.contains("show-advanced") ? "Less options" : "More options";
+async function getFeedData(url = '') {
+  const response = await fetch(url, {
+    //mode: 'no-cors'
+  })
+  return response
 }
 
-function doParse() {
-   var detail = "";
-   if($("detailhide").checked) detail = "&detail=-1";
-   if($("detailshow").checked) detail = "&detail=" + $("detailnum").value;
-
-   var limit = $("limit").checked ? ("&limit=" + $("limitnum").value) : "";
-
-   var advanced = document.body.classList.contains("show-advanced");
-   var showtitle = (advanced && $("showtitle").checked ? "" : "&showtitle=false");
-   var showicon = (advanced && $("showicon").checked ? "&showicon=true" : "");
-   var showempty = (advanced && $("showempty").checked ? "&showempty=true" : "");
-   var striphtml = (advanced && $("striphtml").checked ? "&striphtml=true" : "");
-   var forceutf8 = (advanced && $("forceutf8").checked ? "&forceutf8=true" : "");
-   var fixbugs = (advanced && $("fixbugs").checked ? "&fixbugs=true" : "");
-
-   var path = "/?url=" + encodeURIComponent($("url").value) + detail + limit + showtitle + showicon + showempty + striphtml + forceutf8 + fixbugs;
-   var url = "//rss.bloople.net" + path;
-   var code = "";
-
-   var type = $("form").elements["codegen"].value;
-   if(type == "php") code = "<?php\ninclude(\"https:" + url + "\");\nphp?>";
-   else if(type == "js") code = "<script src=\"" + url + "&type=js\"></script>";
-   else if(type == "html") code = "<iframe src=\"" + url + "&type=html\"></iframe>";
-
-   $("codeout").value = code;
-   $("live-example").src = path;
-
-   document.body.classList.add("submitted");
-
-   return false;
+function createFeedListHtml(data = '') {
+  const items = Array.from(data.querySelectorAll("item")).slice(0, 5)
+  let listCount = 0
+  let html = '<ul>'
+  items.forEach(element => {
+    html += `
+    <li><a href="${element.querySelector("link").innerHTML}">
+    ${element.querySelector("title").innerHTML}
+    </a></li>
+    `
+    listCount += 1
+  })
+  html += '</ul>'
+  console.log(listCount + " items listed")
+  return html
 }
 
-window.onload = function() {
-  $("form").onsubmit = doParse;
-
-  $("limitnum").onclick = function() {
-    $("limit").checked = true;
-  };
-
-  $("detailnum").onclick = function() {
-    $("detailshow").checked = true;
-  };
-
-  $("url").focus();
-};
+getFeedData(jpcomRssUrl)
+  .then(response => response.text())
+  .then(textContent => new window.DOMParser().parseFromString(textContent, "text/xml"))
+  .then(data => {
+    const html = createFeedListHtml(data)
+    console.log(html)
+    const targetDiv = document.getElementById('list')
+    targetDiv.insertAdjacentHTML('afterBegin', html)
+  })
