a60fa8bba660e12122802f1a2ec16b87004a87f7
Jaclyn Paul <jaclyn@jaclynpaul.com>
2021-10-04 16:12:31 -0400

    update rss aggregator to list recent ADHDhs and personal blog posts

--- rss-aggregator.js
+++ rss-aggregator.js
@@ -1,12 +1,16 @@
 const corsAnywhereAppend = 'https://cors-anywhere.herokuapp.com/'
 const jpcomRssUrl = corsAnywhereAppend + 'https://www.jaclynpaul.com/feed/'
-const adhdhsRssUrl = 'https://adhdhomestead.net/feed/'
+const adhdhsRssUrl = corsAnywhereAppend + 'https://adhdhomestead.net/feed/'
+const podcastRssUrl = corsAnywhereAppend + 'https://www.patreon.com/rss/ADHDhomestead?auth=cJKustSVnDs8moTqoCiRMLPgBI-4XukX'
+
+const targetDiv = document.getElementById('list')
 
 async function getFeedData(url = '') {
   const response = await fetch(url, {
-    //mode: 'no-cors'
+    mode: 'cors'
   })
-  return response
+  let textContent = await response.text()
+  return new window.DOMParser().parseFromString(textContent, "text/xml")
 }
 
 function createFeedListHtml(data = '') {
@@ -26,12 +30,24 @@ function createFeedListHtml(data = '') {
   return html
 }
 
-getFeedData(jpcomRssUrl)
-  .then(response => response.text())
-  .then(textContent => new window.DOMParser().parseFromString(textContent, "text/xml"))
-  .then(data => {
-    const html = createFeedListHtml(data)
-    console.log(html)
-    const targetDiv = document.getElementById('list')
-    targetDiv.insertAdjacentHTML('afterBegin', html)
-  })
+async function getHtmlForPostList(feedUrl = '') {
+  const data = await getFeedData(feedUrl)
+  return createFeedListHtml(data)
+}
+
+function getHtmlForListTitle(title = '') {
+  let html = `
+    <p>Recent posts at ${title}:</p>
+  `
+  return html
+}
+
+async function addPostListToPage(title = '', feedUrl = '') {
+  let html = ''
+  html += getHtmlForListTitle(title)
+  html += await getHtmlForPostList(feedUrl)
+  targetDiv.insertAdjacentHTML('afterBegin', html)
+}
+
+addPostListToPage('my personal/writing blog', jpcomRssUrl)
+addPostListToPage('The ADHD Homestead', adhdhsRssUrl)
